251c71c1a3632180d1dc576e748a3bccd757253c
Paramvir Dhillon <92112130+Retr0g0D@users.noreply.github.com>
2022-06-20 14:12:15 +0530

    Add files via upload

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Nigger',
+    username: 'Bret',
+    email: 'MUNCHU Fan'
+  },
+  {
+    id: 2,
+    name: 'Fudday',
+    username: 'Antonette',
+    email: 'Dost ke papa ka buisness'
+  },
+  {
+    id: 3,
+    name: 'Gor-hub',
+    username: 'Samantha',
+    email: 'Gaymer'
+  },
+  {
+    id: 4,
+    name: 'Ka-randee-p',
+    username: 'Karianne',
+    email: 'Mehar ka bestie'
+  },
+  {
+    id: 5,
+    name: 'Paramvir',
+    username: 'Kamren',
+    email: 'JOD'
+  },
+  {
+    id: 6,
+    name: 'Mrs. Dennis Schulist',
+    username: 'Leopoldo_Corkery',
+    email: 'Karley_Dach@jasper.info'
+  },
+  {
+    id: 7,
+    name: 'Kurtis Weissnat',
+    username: 'Elwyn.Skiles',
+    email: 'Telly.Hoeger@billy.biz'
+  },
+  {
+    id: 8,
+    name: 'Nicholas Runolfsdottir V',
+    username: 'Maxime_Nienow',
+    email: 'Sherwood@rosamond.me'
+  },
+  {
+    id: 9,
+    name: 'Glenna Reichert',
+    username: 'Delphine',
+    email: 'Chaim_McDermott@dana.io'
+  },
+  {
+    id: 10,
+    name: 'Clementina DuBuque',
+    username: 'Moriah.Stanton',
+    email: 'Rey.Padberg@karina.biz'
+  }
+];
